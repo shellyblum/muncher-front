1c7acb19c4c3bdb87318bd45c384a2321e868d8a
bebe green <binyamin.green@gmail.com>
2017-10-23 13:58:24 +0300

    login seed with redux

--- src/components/Login/login.js
+++ src/components/Login/login.js
@@ -7,33 +7,42 @@ import TextField from 'material-ui/TextField';
 class LoginForm extends Component {
     constructor(props) {
         super(props);
-        this.state = { username: '', password: '' }
+        this.state = { username: '', password: '', usernameError: '', passwordError: '' }
         this.handleInputChange = this.handleInputChange.bind(this);
         this.handleLoginRequest = this.handleLoginRequest.bind(this);
     }
+
     handleInputChange({ target }) {
         this.setState({ [target.name]: target.value })
     }
+
     handleLoginRequest() {
         const { dispatch } = this.props;
         const { username, password } = this.state;
         if (username && password) {
             dispatch(loginRequest(username, password))
         }
+        else {
+            this.setState({ [username ? 'passwordError' : 'usernameError']: 'This field is required.' })
+        }
     }
+
     render() {
+        const { usernameError, passwordError } = this.state;
         return (
             <div style={{ margin: '3em' }}>
                 <TextField
                     name='username'
                     hintText="Username"
+                    errorText={usernameError}
                     onChange={this.handleInputChange}
                 /><br />
                 <TextField
                     name='password'
                     hintText="Password"
-                    onChange={this.handleInputChange}
+                    errorText={passwordError}
                     type='password'
+                    onChange={this.handleInputChange}
                 /><br />
                 <RaisedButton primary onClick={this.handleLoginRequest} label="Login" />
             </div>
