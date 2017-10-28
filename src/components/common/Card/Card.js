7f7e4ac9281c58bec99764b2b51fef84c7de211e
Michael Alaev <alaev.michael@gmail.com>
2017-10-28 21:07:46 +0300

    Added hover effect to Card

--- src/components/common/Card/Card.js
+++ src/components/common/Card/Card.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 import {
   Card,
   CardActions,
@@ -9,17 +10,26 @@ import {
 } from 'material-ui/Card';
 import FlatButton from 'material-ui/FlatButton';
 
+const HoverEffect = styled.div`
+  transition: all 0.3s ease;
+  &:hover {
+    box-shadow: rgba(0, 0, 0, 0.5) 0px 1px 6px, rgba(0, 0, 0, 0.12) 0px 1px 4px;
+  }
+`;
+
 const CardWithImage = ({ image, title, subtitle, text, avatar, action }) => (
-  <Card>
-    <CardHeader title={title} subtitle={subtitle} avatar={avatar} />
-    <CardMedia>
-      <img src={image} alt={title} />
-    </CardMedia>
-    <CardText>{text}</CardText>
-    <CardActions>
-      <FlatButton label={action} />
-    </CardActions>
-  </Card>
+  <HoverEffect>
+    <Card>
+      <CardHeader title={title} subtitle={subtitle} avatar={avatar} />
+      <CardMedia>
+        <img src={image} alt={title} />
+      </CardMedia>
+      <CardText>{text}</CardText>
+      <CardActions>
+        <FlatButton label={action} />
+      </CardActions>
+    </Card>
+  </HoverEffect>
 );
 
 CardWithImage.propTypes = {
@@ -32,13 +42,11 @@ CardWithImage.propTypes = {
 };
 
 CardWithImage.defaultProps = {
-  image:
-    'https://www.equalserving.com/images/es1scplugin/image-not-available.png',
+  image: 'https://www.equalserving.com/images/es1scplugin/image-not-available.png',
   title: 'Title unavailable',
   subtitle: 'Subtitle unavailable',
   text: 'Text unavailable',
-  avatar:
-    'https://www.equalserving.com/images/es1scplugin/image-not-available.png',
+  avatar: 'https://www.equalserving.com/images/es1scplugin/image-not-available.png',
   action: 'Action button'
 };
 
