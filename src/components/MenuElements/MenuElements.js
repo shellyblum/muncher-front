862352d9b89db4c303046ea660bc29c4931667b6
gilbaadani <gilbaadani@gmail.com>
2017-10-29 22:23:04 +0200

    Updated menu

---
+++ src/components/MenuElements/MenuElements.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+import React from 'react';
+import MenuElementChild from '../MenuElementChild/MenuElementChild'
+
+const MenuContent = styled.div`
+margin: 35px 18px;
+font-size: 18px;
+`;
+
+const MenuElements = ({items}) => (
+<MenuContent>
+    {items.map(item => (
+    <MenuElementChild key = {item.price} {...item} />
+     ))}
+</MenuContent>
+);
+
+export default MenuElements;
+
+MenuElements.propTypes = {
+    items:PropTypes.arrayOf(Object).isRequired
+  };
