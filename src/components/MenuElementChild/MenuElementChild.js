963259db0f3f8551e69a6584ee3388dfc2a8a7ca
gilbaadani <gilbaadani@gmail.com>
2017-10-30 18:20:39 +0200

    menu with fix

--- src/components/MenuElementChild/MenuElementChild.js
+++ src/components/MenuElementChild/MenuElementChild.js
@@ -1,49 +1,48 @@
-import React  from 'react';
-import PropTypes from 'prop-types';
+import React from 'react';
 import styled from 'styled-components';
+import PropTypes from 'prop-types';
+
 
 const ItemPrice = styled.div`
-float: right;
-font-weight: bold;
-font-family: arial;
-margin-top: -22px;
+  float: right;
+  font-weight: bold;
+  font-family: arial;
+  margin-top: -22px;
 `;
 
 const ItemDescription = styled.div`
-font-style: italic;
-font-size: 0.9em;
-line-height: 1.5em;
+  font-style: italic;
+  font-size: 0.9em;
+  line-height: 1.5em;
 `;
 
 const ItemName = styled.div`
-font-family: helvetica;
-font-weight: bold;
-border-bottom: 2px dotted rgb(213, 213, 213);
-margin-top: 1em;
+  font-family: helvetica;
+  font-weight: bold;
+  border-bottom: 2px dotted rgb(213, 213, 213);
+  margin-top: 1em;
 `;
 
-const  MenuElementChild = ({name,price,description}) => {
-        return (
-            <div key = {name}> 
-                 <ItemName> {name} </ItemName>
-                 <ItemPrice> {price}</ItemPrice>
-                  <ItemDescription>
-                 {description}
-                 </ItemDescription>
-             </div>
-        );
-}
+const MenuElementChild = ({ id, name, price, description }) => (
+  <div key={id}>
+    <ItemName> {name} </ItemName>
+    <ItemPrice> {price}</ItemPrice>
+    <ItemDescription>{description}</ItemDescription>
+  </div>
+);
 
-export default MenuElementChild;
 
 MenuElementChild.propTypes = {
-    name: PropTypes.string.isRequired,
-    price: PropTypes.string.isRequired,
-    description: PropTypes.string.isRequired
-}
+  id: PropTypes.number.isRequired,
+  name: PropTypes.string,
+  price: PropTypes.string,
+  description: PropTypes.string
+};
 
 MenuElementChild.defaultProps = {
-    name:'name',
-    price:'price',
-    description:'description'
-}
+  name: 'name',
+  price: 'price',
+  description: 'description'
+};
+
+export default MenuElementChild;
