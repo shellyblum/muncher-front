bb366b930105caebce78cfd99e9da679357939ef
Michael Alaev <alaev.michael@gmail.com>
2017-10-31 21:14:25 +0200

    styeld

--- src/components/SignupPage/signupPage.js
+++ src/components/SignupPage/signupPage.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import ReusableForm from '../ReusableForm/reusableForm';
+import ReusableForm from '../ReusableForm/ReusableForm';
 import { loginSignupRequest } from '../../actions/user';
 import messages from '../../helpers/messages';
 
---
+++ src/pages/HomePage/HomePage.styles.js
@@ -0,0 +1,32 @@
+import styled from 'styled-components';
+
+export const Main = styled.div`
+  background-color: #fbfbfb;
+  padding: 1em;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-template-areas: 'search search search' 'left right right'
+    'bottomLeft right right' '. right right';
+`;
+
+export const Search = styled.div`
+  border: 1px solid black;
+  height: 50px;
+  margin-bottom: 5px;
+  grid-area: ${({ gridArea }) => gridArea};
+`;
+
+export const Left = styled.div`
+  border: 1px solid red;
+  height: 450px;
+  margin-bottom: 5px;
+  margin-right: 5px;
+  grid-area: ${({ gridArea }) => gridArea};
+`;
+
+export const BottomLeft = styled.div`
+  border: 1px solid green;
+  margin-right: 5px;
+  grid-area: ${({ gridArea }) => gridArea};
+  height: 250px;
+`;
