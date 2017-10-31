333626f9716eed9db03caa6bbed3c113c7654f03
gilbaadani <gilbaadani@gmail.com>
2017-10-31 21:51:12 +0200

    menu after refactoring 2

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -4,12 +4,12 @@ import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import MenuElements from '../MenuElements/MenuElements';
 
-const data = {
-  drinks: [{ id: 1, name: 'vodka', price: '50', currency: '$', description: 'lorem ipsum' }, { id: 2, name: 'taqila', price: '90', currency: '$', description: 'lorem ipsum' }, { id: 3, name: 'water', price: '10', currency: '$', description: 'lorem ipsum' }],
-  Breakfast: [{ id: 4, name: 'eggs', price: '89', currency: '$', description: 'lorem ipsum' }, { id: 5, name: 'bacon', price: '6', currency: '$', description: 'lorem ipsum' }],
-  mains: [{ id: 6, name: 'hamburger', price: '89', currency: '$', description: 'lorem ipsum' }, { id: 7, name: 'salmon', price: '25', currency: '$', description: 'lorem ipsum' }],
-  snacks: [{ id: 8, name: 'white chocolate', price: '240', currency: '$', description: 'lorem ipsum' }, { id: 9, name: 'coconut', price: '49', currency: '$', description: 'lorem ipsum' }, { id: 10, name: 'hash cake', price: '499', currency: '$', description: 'lorem ipsum' }]
-};
+const data = [
+  { category: 'drinks', list: [{ id: 1, name: 'vodka', price: '50', currency: '$', description: 'lorem ipsum' }, { id: 2, name: 'taqila', price: '90', currency: '$', description: 'lorem ipsum' }, { id: 3, name: 'water', price: '10', currency: '$', description: 'lorem ipsum' }] },
+  { category: 'Breakfast', list: [{ id: 4, name: 'eggs', price: '89', currency: '$', description: 'lorem ipsum' }, { id: 5, name: 'bacon', price: '6', currency: '$', description: 'lorem ipsum' }] },
+  { category: 'mains', list: [{ id: 6, name: 'hamburger', price: '89', currency: '$', description: 'lorem ipsum' }, { id: 7, name: 'salmon', price: '25', currency: '$', description: 'lorem ipsum' }] },
+  { category: 'snacks', list: [{ id: 8, name: 'white chocolate', price: '240', currency: '$', description: 'lorem ipsum' }, { id: 9, name: 'coconut', price: '49', currency: '$', description: 'lorem ipsum' }, { id: 10, name: 'hash cake', price: '499', currency: '$', description: 'lorem ipsum' }] }
+];
 
 const Body = styled.div`
   width: ${({ width }) => width || '100px'};
@@ -34,11 +34,9 @@ class Menu extends React.Component {
             });
           }}
         >
-          {Object.keys(data).map(key => (
-            <Tab key={key} label={key} value={key}>
-              <div>
-                <MenuElements items={data[key]}>{key}</MenuElements>
-              </div>
+          {data.map((item) => (
+            <Tab key={item.category} label={item.category} value={item.category}>
+              <MenuElements items={item.list}>{item.list.name}</MenuElements>
             </Tab>
           ))}
         </Tabs>
