201c46ef82b995ede758f4b2f84ebaded81cd803
Michael Alaev <alaev.michael@gmail.com>
2017-10-31 21:09:00 +0200

    rename

---
+++ src/components/ReusableForm/ReusableForm.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
+import Form from './ReusableForm.style';
+
+const ReusableForm = ({
+  fields,
+  errors,
+  handleInputChange,
+  handleSubmit,
+  submitLabel
+}) => (
+  <Form>
+    {fields.map(field => (
+      <TextField
+        key={field.name}
+        name={field.name}
+        type={field.type}
+        floatingLabelText={field.placeHolder}
+        errorText={errors[`${field.name}Error`]}
+        onChange={handleInputChange}
+      />
+    ))}
+    <RaisedButton primary onClick={handleSubmit} label={submitLabel} />
+  </Form>
+);
+
+ReusableForm.propTypes = {
+  fields: PropTypes.arrayOf(Object).isRequired,
+  errors: PropTypes.objectOf(String).isRequired,
+  handleInputChange: PropTypes.func.isRequired,
+  handleSubmit: PropTypes.func.isRequired,
+  submitLabel: PropTypes.string.isRequired
+};
+
+export default ReusableForm;
