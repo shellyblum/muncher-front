10779b53312caf6fce942615b104ab6c9c1380ae
Michael Alaev <alaev.michael@gmail.com>
2017-10-31 21:04:50 +0200

    styles

---
+++ src/components/Header/Header.styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const HeaderStyle = styled.header`
+  padding: 1em;
+  display: flex;
+  align-items: center;
+`;
+
+export const Title = styled.h1`
+  color: #cb0067;
+  margin: 0 1em;
+`;
