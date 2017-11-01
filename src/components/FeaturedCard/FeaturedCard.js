a6708e6a9c9979829b8b5559816687b914f99b48
Andrey Kishtov <andreykishtov@gmail.com>
2017-11-01 18:35:47 +0200

    added data file

--- src/components/FeaturedCard/FeaturedCard.js
+++ src/components/FeaturedCard/FeaturedCard.js
@@ -1,60 +1,7 @@
 import React from 'react';
 import { Tabs, Tab } from 'material-ui/Tabs';
 import FontIcon from 'material-ui/FontIcon';
-import FeaturedRest from './components/FeaturedRest';
-import FreshOnSite from './components/FreshOnSite';
-import Special from './components/Special';
-
-const data = [
-  {
-    image:
-      'http://www.telegraph.co.uk/content/dam/Travel/hotels/europe/spain/canary-islands/tenerife/h10-timanfaya-palace-lanzarote-restaurant-small.jpg',
-    title: 'Muncher - Title4',
-    subtitle: 'Muncher - Subtitle4',
-    text:
-      'Muncher - Text4: It is a long established fact that a reader will be distracted.',
-    avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/samihah/128.jpg',
-    action: 'Muncher - action'
-  },
-  {
-    image:
-      'http://www.telegraph.co.uk/content/dam/Travel/hotels/europe/spain/canary-islands/tenerife/h10-timanfaya-palace-lanzarote-restaurant-small.jpg',
-    title: 'Muncher - Title5',
-    subtitle: 'Muncher - Subtitle5',
-    text:
-      'Muncher - Text5: It is a long established fact that a reader will be distracted.',
-    avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/samihah/128.jpg',
-    action: 'Muncher - action'
-  },
-  {
-    image:
-      'http://www.telegraph.co.uk/content/dam/Travel/hotels/europe/spain/canary-islands/tenerife/h10-timanfaya-palace-lanzarote-restaurant-small.jpg',
-    title: 'Muncher - Title6',
-    subtitle: 'Muncher - Subtitle6',
-    text:
-      'Muncher - Text6: It is a long established fact that a reader will be distracted.',
-    avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/samihah/128.jpg',
-    action: 'Muncher - action'
-  }
-];
-
-const dataForWidget = [
-  {
-    label: 'Featured list',
-    name: 'fa fa-star',
-    component: <FeaturedRest data={data} />
-  },
-  {
-    label: 'Fresh on site',
-    name: 'fa fa-bolt',
-    component: <FreshOnSite data={data} />
-  },
-  {
-    label: 'Todays special',
-    name: 'fa fa-money',
-    component: <Special data={data[0]} />
-  }
-];
+import { dataForWidget } from './data';
 
 const FeaturedCard = () => (
   <Tabs>
