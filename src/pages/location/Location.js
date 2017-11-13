885c804ca48432289a4e6f6636186c79849342f7
costa Shush <shushkovc@gmail.com>
2017-11-13 14:19:20 +0200

    image show in location

--- src/pages/location/Location.js
+++ src/pages/location/Location.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import styled from 'styled-components';
 import Menu from '../../components/Menu/Menu';
+import ImageShow from '../../components/ImageShow/ImageShow';
+import images from './images';
+
+const LocationWrapper = styled.div`
+display:grid;
+grid-template-columns:  2fr 1fr;
+grid-gap: 1rem;
+padding : 1rem;
+`;
 
 const location = () => (
   <div>
-    <Menu grid="400px" />
+    <LocationWrapper>
+      <ImageShow data={images} />
+      <Menu grid="400px" />
+    </LocationWrapper>
   </div>
 );
 
