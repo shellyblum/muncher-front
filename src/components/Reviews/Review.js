af87e712564f47f76ba8094a74defe8a312cabd3
Michael Alaev <Alaev@users.noreply.github.com>
2017-11-14 20:49:21 +0200

    lint
    added indent

--- src/components/Reviews/Review.js
+++ src/components/Reviews/Review.js
@@ -3,20 +3,20 @@ import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
 const ImageWrapp = styled.img`
-height: 60px;
-width: 60px;
-background-color: #d7e4ed;
-border: 3px solid grey;
-border-radius:30px;
-position: absolute;
-margin-top: 20px;
+  height: 60px;
+  width: 60px;
+  background-color: #d7e4ed;
+  border: 3px solid grey;
+  border-radius:30px;
+  position: absolute;
+  margin-top: 20px;
 `;
 const ReviewWrapper = styled.div`
-margin-top: 2em;
-box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
-padding: 40px;
-border-radius: 10px;
-margin-left: 100px;
+  margin-top: 2em;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  padding: 40px;
+  border-radius: 10px;
+  margin-left: 100px;
 `;
 
 
