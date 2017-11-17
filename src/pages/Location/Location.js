8a75e71955f66cb0bb3a4eabdb08388c7627e8e1
Michael Alaev <alaev.michael@gmail.com>
2017-11-17 19:53:10 +0200

    merge

--- src/pages/Location/Location.js
+++ src/pages/Location/Location.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import styled from 'styled-components';
 import Menu from '../../components/Menu/Menu';
+import ImageShow from '../../components/ImageShow/ImageShow';
+import ReviewList from '../../components/Reviews/ReviewList';
+import images from './images';
+import reviews from './reviews';
+
+const LocationWrapper = styled.div`
+display:grid;
+grid-template-columns:  1fr 2fr;
+grid-gap: 1rem;
+padding : 1rem;
+`;
 
 const location = () => (
   <div>
-    <Menu grid="400px" />
+    <LocationWrapper>
+      <ImageShow data={images} />
+      <Menu grid="2fr" />
+      <ReviewList data={reviews} />
+    </LocationWrapper>
   </div>
 );
 
