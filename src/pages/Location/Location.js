4f8fdc91c38d5b5b4f8e276271857810d8580ecb
Yuval <uvluni@gmail.com>
2017-11-19 16:30:16 +0200

    changed export default to images and reviews

--- src/pages/Location/Location.js
+++ src/pages/Location/Location.js
@@ -7,12 +7,14 @@ import images from './images';
 import reviews from './reviews';
 
 const LocationWrapper = styled.div`
-  display:grid;
-  grid-template-columns:  1fr 2fr;
+  display: grid;
+  grid-template-columns: 1fr 2fr;
   grid-gap: 1rem;
-  padding : 1rem;
+  padding: 1rem;
 `;
 
+console.log(images);
+console.log(reviews);
 const location = () => (
   <div>
     <LocationWrapper>
