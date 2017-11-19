bdb7632d98290c2cf29c2e750a500546b9138d98
Yuval <uvluni@gmail.com>
2017-11-19 16:18:17 +0200

    card and grid are now and with basic design

--- src/components/CardWithImage/CardWithImage.js
+++ src/components/CardWithImage/CardWithImage.js
@@ -1,37 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Card, CardActions, CardMedia, CardText } from 'material-ui/Card';
-import FlatButton from 'material-ui/FlatButton';
-import { red100, deepPurple100 } from 'material-ui/styles/colors';
-import {
-  HoverEffect,
-  styleCardText,
-  styleFlatButton,
-  styleCard
-} from '../CardWithImage/CardWithImage.styled';
 
-const CardWithImage = ({ id, image, title, text, action, lat, lng, selected, toggleCTADialog }) => (
-  <HoverEffect selected={selected}>
-    <Card style={styleCard}>
-      <CardText style={styleCardText}>{title}</CardText>
-      <CardMedia>
-        <img src={image} alt={title} />
-      </CardMedia>
-      <CardText>{text}</CardText>
-      <CardActions style={styleFlatButton}>
-        <FlatButton
-          primary
-          hoverColor={red100}
-          label={action}
-          onClick={() => toggleCTADialog(id)}
-        />
-        <FlatButton primary hoverColor={deepPurple100} label="Map" />
-      </CardActions>
-      <button onClick={() => {}}>
-        {lat} ,{lng},{id}
-      </button>
-    </Card>
-  </HoverEffect>
+import { SelectEffect } from '../CardWithImage/CardWithImage.styled';
+
+const CardWithImage = ({ id, image, title, text, action, selected, toggleCTADialog }) => (
+  <SelectEffect selected={selected}>
+    <h3>{title}</h3>
+    <div className="custom-image">
+      <img alt={title} width="100%" src={image} />
+    </div>
+    <div className="custom-card">
+      <p>{text}</p>
+    </div>
+    <button onClick={() => toggleCTADialog(id)}> {action} </button>
+    <button
+      onClick={e => {
+        console.log(e.target);
+      }}
+    >
+      Show on map
+    </button>
+  </SelectEffect>
 );
 
 CardWithImage.propTypes = {
@@ -40,8 +29,6 @@ CardWithImage.propTypes = {
   title: PropTypes.string,
   text: PropTypes.string,
   action: PropTypes.string,
-  lat: PropTypes.number.isRequired,
-  lng: PropTypes.number.isRequired,
   selected: PropTypes.string,
   toggleCTADialog: PropTypes.func
 };
