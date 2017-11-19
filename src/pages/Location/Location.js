6d040f7765fb0950c2097e8bb97dabeeb3d20f5e
Yuval <uvluni@gmail.com>
2017-11-19 16:44:15 +0200

    changed to exporting an object

--- src/pages/Location/Location.js
+++ src/pages/Location/Location.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import styled from 'styled-components';
-import images from './images';
-import reviews from './reviews';
 import Menu from '../../components/Menu/Menu';
 import ImageShow from '../../components/ImageShow/ImageShow';
 import ReviewList from '../../components/Reviews/ReviewList';
+import image from './image';
+import review from './review';
 
 const LocationWrapper = styled.div`
   display: grid;
@@ -13,14 +13,14 @@ const LocationWrapper = styled.div`
   padding: 1rem;
 `;
 
-console.log(images);
-console.log(reviews);
+console.log(image);
+console.log(review);
 const location = () => (
   <div>
     <LocationWrapper>
-      <ImageShow data={images.images} />
+      <ImageShow data={image.images} />
       <Menu grid="2fr" />
-      <ReviewList data={reviews.reviews} />
+      <ReviewList data={review.reviews} />
     </LocationWrapper>
   </div>
 );
