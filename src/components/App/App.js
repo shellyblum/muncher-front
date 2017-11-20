23ae4bb58cccde840a40b77d53a4c666a4c2cc11
Yuval <uvluni@gmail.com>
2017-11-20 10:12:13 +0200

    added back the Location route

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,6 +4,7 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import { injectGlobal } from 'styled-components';
 import SignupPage from '../../components/SignupPage/SignupPage';
 import HomePage from '../../pages/HomePage/HomePage';
+import Location from '../../pages/Location/Location';
 import DashBoardPage from '../../containers/DashBoardPage';
 import Header from '../Header/Header';
 
@@ -22,6 +23,7 @@ const App = () => (
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/signup" component={SignupPage} />
+          <Route path="/location" component={Location} />
           <Route exact path="/dashboard" component={DashBoardPage} />
           <Route component={() => <h1>Oops.. page not found</h1>} />
         </Switch>
---
+++ src/pages/Location/Location.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import styled from 'styled-components';
+import Menu from '../../components/Menu/Menu';
+import ImageShow from '../../components/ImageShow/ImageShow';
+import ReviewList from '../../components/Reviews/ReviewList';
+import images from './images';
+import reviews from './reviews';
+
+const LocationWrapper = styled.div`
+  display:grid;
+  grid-template-columns:  1fr 2fr;
+  grid-gap: 1rem;
+  padding : 1rem;
+`;
+
+const location = () => (
+  <div>
+    <LocationWrapper>
+      <ImageShow data={images} />
+      <Menu grid="2fr" />
+      <ReviewList data={reviews} />
+    </LocationWrapper>
+  </div>
+);
+
+export default location;
