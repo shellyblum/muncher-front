6fa19af2e081f061c7670db4e9f99dfea7fab70f
Andrey Kishtov <andreykishtov@gmail.com>
2017-11-22 14:55:02 +0200

    changing special

--- src/components/FeaturedCard/FeaturedCard.js
+++ src/components/FeaturedCard/FeaturedCard.js
@@ -1,20 +1,32 @@
 import React from 'react';
 import 'antd/lib/card/style/css';
-import { Tabs, Icon } from 'antd';
+import { Tabs } from 'antd';
+import styled from 'styled-components';
 import { dataForWidget } from './data';
 
 const { TabPane } = Tabs;
+
+const StyledTab = styled.span`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+`;
+
+const Span = styled.span``;
+
+const I = styled.i``;
 
 const FeaturedCard = () => (
   <Tabs defaultActiveKey="2" tabPosition="left">
     {dataForWidget.map(item => (
       <TabPane
+        tabBarStyle={{ margin: '0px' }}
         key={item.label}
         tab={
-          <span>
-            <Icon type={item.name} />
-            {item.label}
-          </span>
+          <StyledTab>
+            <Span>{item.label}</Span>
+            <I className={item.name} aria-hidden="true" />
+          </StyledTab>
         }
       >
         {item.component}
