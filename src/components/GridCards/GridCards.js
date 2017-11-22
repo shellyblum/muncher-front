8c7025e08a0dfcff7b495831b7d6eba87c2f0f15
Yuval <uvluni@gmail.com>
2017-11-22 11:43:00 +0200

    Changed name of component GridListCards to GridList for easier reading

--- src/components/GridCards/GridCards.js
+++ src/components/GridCards/GridCards.js
@@ -7,7 +7,7 @@ import { Card } from 'antd';
 
 import LocationCard from '../LocationCard/LocationCard';
 
-const GridListCards = ({ filteredCards, toggleCTADialog }) => (
+const GridCards = ({ filteredCards, toggleCTADialog }) => (
   <Card bordered={false} noHovering>
     {filteredCards.map(tile => (
       <Card.Grid
@@ -20,7 +20,7 @@ const GridListCards = ({ filteredCards, toggleCTADialog }) => (
   </Card>
 );
 
-GridListCards.propTypes = {
+GridCards.propTypes = {
   toggleCTADialog: PropTypes.func,
   filteredCards: PropTypes.arrayOf(PropTypes.shape({
     image: PropTypes.string,
@@ -34,8 +34,8 @@ GridListCards.propTypes = {
   })).isRequired
 };
 
-GridListCards.defaultProps = {
+GridCards.defaultProps = {
   toggleCTADialog: () => null
 };
 
-export default GridListCards;
+export default GridCards;
