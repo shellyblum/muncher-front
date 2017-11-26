905b12f93eb7de512e3f553ec5cef61ffbe8e44e
costa Shush <shushkovc@gmail.com>
2017-11-26 15:23:20 +0200

    fix margin directions doubles

--- src/components/Reviews/Review.js
+++ src/components/Reviews/Review.js
@@ -3,11 +3,10 @@ import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
 const ReviewWrapper = styled.div`
-  margin-top: 2em;
+  margin: 2em 0 0 10px;
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
   padding: 20px;
   border-radius: 10px;
-  margin-left: 10px;
 `;
 
 function Review({ title, content, date }) {
